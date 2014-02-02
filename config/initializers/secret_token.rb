a970a5d4c8cf341678cf5620608683adad04fb3f
ynagarjuna1995 <ynagarjuna2012@gmail.com>
2014-02-02 16:32:27 +0530

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arcmagnus1::Application.config.secret_token = '0c27ea7f6cddb7e7247b9c46b34266dcba71658cae5c1b86d3687153b2aedf8fcee4f811f0ef0360666e2a19e1c80af1ff68cc93c21e6882ee3ef6c7226465c0'
